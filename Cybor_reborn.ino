2d12c29d1fe73e05abf28be94fb6185766008e9f
Susensio <susensio@gmail.com>
2017-12-15 01:42:11 +0100

    Running

--- Cybor_reborn.ino
+++ Cybor_reborn.ino
@@ -1,23 +1,22 @@
 #include <Wire.h>
 
-
 // H-Bridge motor pins                      7 pin socket driver
 const byte MOTOR_RIGHT_FORWARDS_PIN = 11;   // 1. Blue
 const byte MOTOR_RIGHT_BACKWARDS_PIN = 10;  // 2. Grey
 const byte MOTOR_LEFT_FORWARDS_PIN = 9;     // 3. Green
-const byte MOTOR_LEFT_BACKWARDS_PIN = 6;    // 4. White
-
-// LED Antennas
+const byte MOTOR_LEFT_BACKWARDS_PIN = 6;    // 4. White// LED Antennas
 const int LED_LEFT_PIN = 2;
 const int LED_RIGHT_PIN = 3;
 
-boolean led_left = false;
-boolean led_right = false;
+// Robot variables
+boolean ledLeft = false;
+boolean ledRight = false;
+int linearVelocity = 0;
+int angularVelocity = 0;
 
-byte cmd;
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   
   pinMode(MOTOR_RIGHT_FORWARDS_PIN,OUTPUT);
   pinMode(MOTOR_RIGHT_BACKWARDS_PIN,OUTPUT);
@@ -27,56 +26,83 @@ void setup() {
   pinMode(LED_LEFT_PIN,OUTPUT);
   pinMode(LED_RIGHT_PIN,OUTPUT);
 
-  Wire.begin(8);                // join i2c bus with address #8
+  Wire.begin(8);           // join i2c bus with address #8
   Wire.onReceive(readI2C); // register event
   
-  Serial.println("Hola");
+  //Serial.println("Hola");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char c = Serial.read();
-    switch (c){
-      case 'a': digitalWrite(LED_LEFT_PIN,LOW); break;
-      case 'q': digitalWrite(LED_LEFT_PIN,HIGH); break;
-      case 'd': digitalWrite(LED_RIGHT_PIN,LOW); break;
-      case 'e': digitalWrite(LED_RIGHT_PIN,HIGH); break;
-      default: break;
-    }
-  }
-  
-  digitalWrite(LED_LEFT_PIN,led_left);
-  digitalWrite(LED_RIGHT_PIN,led_right);
+  digitalWrite(LED_LEFT_PIN,ledLeft);
+  digitalWrite(LED_RIGHT_PIN,ledRight);
+  reference(linearVelocity, angularVelocity);
 }
 
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
+
 void readI2C(int howMany) {
-  Serial.print(__FUNCTION__);
-  cmd = Wire.read();
+  char cmd = Wire.read();
   if (Wire.available()){  // More data
-    char result[howMany-1];
+    byte result[howMany-1];
     int i = 0;
     while (Wire.available()) {
       result[i] = Wire.read();
       i++;
     }
     switch (cmd){
-      case 'L': updateLed(result); break;
+      case 'L': updateLeds(result[0]); break;
+      case 'M': updateMotors(result); break;
       default: break;
     }
   }
 }
 
-void updateLed(char result[]){
-  Serial.print(__FUNCTION__);
-  Serial.println(result);
-  switch (result[0]){
-    case '0': led_left = LOW; break;
-    case '1': led_left = HIGH; break;
-    case '2': led_right = LOW; break;
-    case '3': led_right = HIGH; break;
-    default: break;
+void updateLeds(byte result){
+  ledLeft = result & B00000010;
+  ledRight = result & B00000001;
+}
+
+void updateMotors(byte result[]){
+  linearVelocity = (result[0]<<8) + result[1];
+  angularVelocity = (result[2]<<8) + result[3];
+}
+
+void reference(int v, int w){
+  v = v/80;
+  w = w/20;
+  int leftMotor=constrain( v-w ,-255,255);
+  int rightMotor=constrain( v+w ,-255,255);
+  
+  motorSpeed(leftMotor,rightMotor);
+}
+
+
+void motorSpeed(int left, int right){
+  leftMotorSpeed(left);
+  rightMotorSpeed(right);
+}
+
+void rightMotorSpeed(int speed){
+  if (speed>0){
+    digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,LOW);
+    analogWrite(MOTOR_RIGHT_FORWARDS_PIN,speed);
+  } else if (speed<0){
+    digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,LOW);
+    analogWrite(MOTOR_RIGHT_BACKWARDS_PIN,-speed);
+  } else{
+    digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,LOW);
+    digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,LOW);
   }
 }
 
+void leftMotorSpeed(int speed){
+  if (speed>0){
+    digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,LOW);
+    analogWrite(MOTOR_LEFT_FORWARDS_PIN,speed);
+  } else if (speed<0){
+    digitalWrite(MOTOR_LEFT_FORWARDS_PIN,LOW);
+    analogWrite(MOTOR_LEFT_BACKWARDS_PIN,-speed);
+  } else{
+    digitalWrite(MOTOR_LEFT_FORWARDS_PIN,LOW);
+    digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,LOW);
+  }
+}
