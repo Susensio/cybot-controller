17faccfe3db7b37d78325f69ecae476b21157320
Susensio <susensio@gmail.com>
2017-12-22 14:11:48 +0100

    Add control references

--- cybot-controller.ino
+++ cybot-controller.ino
@@ -16,28 +16,42 @@
  #define DEBUG_PRINTLN(x)
 #endif
 
-MPU6050 mpu;
+// H-Bridge motor pins                      7 pin socket driver
+const byte MOTOR_RIGHT_FORWARDS_PIN = 11;   // 1. Blue
+const byte MOTOR_RIGHT_BACKWARDS_PIN = 10;  // 2. Grey
+const byte MOTOR_LEFT_FORWARDS_PIN = 9;     // 3. Green
+const byte MOTOR_LEFT_BACKWARDS_PIN = 6;    // 4. White// LED Antennas
 
+const int LED_LEFT_PIN = 2;
+const int LED_RIGHT_PIN = 3;
+
+// MPU variables
 const byte INTERRUPT_PIN = 2;  // use pin 2 on Arduino Uno & most boards
-
+MPU6050 mpu;
 volatile uint8_t fifoBuffer[14]; // FIFO storage buffer
 
+// Robot variables
+boolean ledLeft = false;
+boolean ledRight = false;
+int linearVelocityRef = 0;
+int angularVelocityRef = 0;
 
-unsigned long interval = 100;
-unsigned long previousTime;
-
-void control();
-Task controlTask(100, TASK_FOREVER, &control);
+// Scheduler
+void controlCallback();
+Task controlTask(100, TASK_FOREVER, &controlCallback);
 Scheduler runner;
 
 
-void control() {
+void controlCallback() {
     static int lastYaw;
 
     int yaw = getYaw();
     DEBUG_PRINTLN(yaw);
 
-    int angularVelocity = yaw - lastYaw;
+    int angularVelocity = (yaw - lastYaw);
+
+    setReference(linearVelocityRef, 2*angularVelocityRef - angularVelocity);
+
 
     DEBUG_PRINTLN(angularVelocity);
     lastYaw = yaw;
@@ -47,8 +61,10 @@ void setup() {
     // initialize serial communication
     Serial.begin(115200);
 
+    motorsSetup();
+    ledsSetup();
+
     mpuSetup();
-    previousTime = millis();
 }
 
 
@@ -56,6 +72,65 @@ void loop() {
     runner.execute();
 }
 
+
+int getYaw() {
+    noInterrupts();
+    int w = ((fifoBuffer[0] << 8) | fifoBuffer[1]);
+    int x = ((fifoBuffer[4] << 8) | fifoBuffer[5]);
+    int y = ((fifoBuffer[8] << 8) | fifoBuffer[9]);
+    int z = ((fifoBuffer[12] << 8) | fifoBuffer[13]);
+    interrupts();
+
+    DEBUG_PRINTLN(w);
+    DEBUG_PRINTLN(x);
+    DEBUG_PRINTLN(y);
+    DEBUG_PRINTLN(z);
+    DEBUG_PRINTLN();
+
+    //atan2(2*x*y - 2*w*z, 2*w*w + 2*x*x - 1);  all divided by 4
+    int yaw = (int)fxpt_atan2((q15_mul(x,y)/2 - q15_mul(w,z)/2),
+                    (q15_mul(w,w)/2 + (q15_mul(x,x) - 0x4000)/2));
+
+    return yaw;
+}
+
+// Reference values in range [-1024:+1024] aprox
+void setReference(int v, int w){
+ int leftMotorSpeed=constrain( (v-w)/4 ,-255,255);
+ int rightMotorSpeed=constrain( (v+w)/4 ,-255,255);
+ 
+ setRightMotorSpeed(rightMotorSpeed);
+ setLeftMotorSpeed(leftMotorSpeed);
+}
+
+void setRightMotorSpeed(int speed){
+ if (speed>0){
+   digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,LOW);
+   analogWrite(MOTOR_RIGHT_FORWARDS_PIN,speed);
+ } else if (speed<0){
+   digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,LOW);
+   analogWrite(MOTOR_RIGHT_BACKWARDS_PIN,-speed);
+ } else{
+   digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,LOW);
+   digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,LOW);
+ }
+}
+
+void setLeftMotorSpeed(int speed){
+ if (speed>0){
+   digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,LOW);
+   analogWrite(MOTOR_LEFT_FORWARDS_PIN,speed);
+ } else if (speed<0){
+   digitalWrite(MOTOR_LEFT_FORWARDS_PIN,LOW);
+   analogWrite(MOTOR_LEFT_BACKWARDS_PIN,-speed);
+ } else{
+   digitalWrite(MOTOR_LEFT_FORWARDS_PIN,LOW);
+   digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,LOW);
+ }
+}
+
+
+/************* SETUP ROUTINES *************/
 
 void mpuSetup() {
     Wire.begin();
@@ -94,7 +169,21 @@ void mpuSetup() {
     DEBUG_PRINTLN(F("DMP ready! Waiting for first interrupt..."));
 }
 
-// Interrupt routine
+void motorsSetup() {
+    pinMode(MOTOR_RIGHT_FORWARDS_PIN,OUTPUT);
+    pinMode(MOTOR_RIGHT_BACKWARDS_PIN,OUTPUT);
+    pinMode(MOTOR_LEFT_FORWARDS_PIN,OUTPUT);
+    pinMode(MOTOR_LEFT_BACKWARDS_PIN,OUTPUT);
+}
+
+void ledsSetup() {
+    pinMode(LED_LEFT_PIN,OUTPUT);
+    pinMode(LED_RIGHT_PIN,OUTPUT);
+}
+
+
+/*********** INTERRUPT ROUTINES ***********/
+
 void dmpDataReady() {
     // read a quaternions from FIFO
     mpu.getFIFOBytes(fifoBuffer, 14);
@@ -103,23 +192,3 @@ void dmpDataReady() {
         
 }
 
-int getYaw() {
-    noInterrupts();
-    int w = ((fifoBuffer[0] << 8) | fifoBuffer[1]);
-    int x = ((fifoBuffer[4] << 8) | fifoBuffer[5]);
-    int y = ((fifoBuffer[8] << 8) | fifoBuffer[9]);
-    int z = ((fifoBuffer[12] << 8) | fifoBuffer[13]);
-    interrupts();
-
-    DEBUG_PRINTLN(w);
-    DEBUG_PRINTLN(x);
-    DEBUG_PRINTLN(y);
-    DEBUG_PRINTLN(z);
-    DEBUG_PRINTLN();
-
-    //atan2(2*x*y - 2*w*z, 2*w*w + 2*x*x - 1);  all divided by 4
-    int yaw = (int)fxpt_atan2((q15_mul(x,y)/2 - q15_mul(w,z)/2),
-                    (q15_mul(w,w)/2 + (q15_mul(x,x) - 0x4000)/2));
-
-    return yaw;
-}
