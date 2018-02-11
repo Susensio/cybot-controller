7bb2066d33802e0ccdb11627509993e1f982908e
Susensio <susensio@gmail.com>
2018-02-11 21:38:51 +0100

    Added SPI communication

--- cybot-controller.ino
+++ cybot-controller.ino
@@ -2,29 +2,50 @@
 #include <I2Cdev.h>
 #include <MPU6050_6Axis_MotionApps20.h>
 
+#include <SPI.h>
+
 #include <TaskScheduler.h>
+
 
 //#include "fxpt_atan2.h"
 
-// H-Bridge motor pins                      7 pin socket driver
-const byte MOTOR_RIGHT_FORWARDS_PIN = 11;   // 1. Blue
-const byte MOTOR_RIGHT_BACKWARDS_PIN = 10;  // 2. Grey
-const byte MOTOR_LEFT_FORWARDS_PIN = 9;     // 3. Green
-const byte MOTOR_LEFT_BACKWARDS_PIN = 6;    // 4. White// LED Antennas
+// L298 N Motor driver pins
+const byte MOTOR_RIGHT_ENABLE_PIN = 9;     // ENA
+const byte MOTOR_RIGHT_FORWARDS_PIN = 8;    // IN1
+const byte MOTOR_RIGHT_BACKWARDS_PIN = 7;   // IN2
 
-const int LED_LEFT_PIN = 2;
-const int LED_RIGHT_PIN = 3;
+const byte MOTOR_LEFT_ENABLE_PIN = 6;       // ENB
+const byte MOTOR_LEFT_FORWARDS_PIN = 5;     // IN3
+const byte MOTOR_LEFT_BACKWARDS_PIN = 4;    // IN4
 
-// Interrupt
-const byte INTERRUPT_PIN = 2;  // use pin 2 on Arduino Uno & most boards
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
-}
+const int LED_LEFT_PIN = A0;
+const int LED_RIGHT_PIN = A1;
 
 // MPU variables
 MPU6050 mpu;
 volatile uint8_t fifoBuffer[14]; // FIFO storage buffer
+
+// SPI variables
+volatile char spiBuffer [10];
+volatile byte pos;
+volatile byte parityByte;
+
+// Interrupts
+const byte I2C_INTERRUPT_PIN = 2;  // use pin 2 on Arduino Uno & most boards
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+const byte SPI_INTERRUPT_PIN = 3;
+volatile bool spiInterrupt;
+void spiDataReady() {
+    spiInterrupt = true;
+}
+ISR (SPI_STC_vect) {
+  spiBuffer [pos++] = SPDR; // grab byte from SPI Data Register
+}
+
+
 
 // Robot variables
 boolean ledLeft = false;
@@ -32,47 +53,96 @@ boolean ledRight = false;
 int linearVelocityRef = 0;
 int angularVelocityRef = 0;
 
+// Control variables
+int sampling_time = 100;
+int K_regulator = 4;
+
 // Scheduler
 void controlCallback();
-Task controlTask(100, TASK_FOREVER, &controlCallback);
+Task controlTask(sampling_time, TASK_FOREVER, &controlCallback);
 Scheduler runner;
-
-
 
 Quaternion q;           // [w, x, y, z]         quaternion container
 
-
 void controlCallback() {
-    static float lastYaw;
-
     float yaw = getYaw();
-    //Serial.println(yaw);
+    float angularVelocity = getAngularVelocity(yaw);
+    
+    //Serial.println (linearVelocityRef);
+    //Serial.println (angularVelocityRef);
+    setReference(linearVelocityRef, angularVelocityRef + K_regulator*(angularVelocityRef - 1.3*angularVelocity));
 
-    float angularVelocity = (yaw - lastYaw);
 
-    setReference(linearVelocityRef, 2*angularVelocityRef - angularVelocity);
-
-
-    Serial.println(angularVelocity);
-    lastYaw = yaw;
+//    Serial.print(yaw);
+//    Serial.print('\t');
+//    Serial.print(angularVelocity);
+//    Serial.print('\t');
+//    Serial.print(linearVelocityRef);
+//    Serial.print('\t');
+//    Serial.println(angularVelocityRef);
 }
 
 void setup() {
     // initialize serial communication
     Serial.begin(115200);
 
-    motorsSetup();
-    ledsSetup();
-
+    spiSetup();
     mpuSetup();
+    //ledsSetup();
     
+    delay(500);
+    motorsSetup();
+
+    delay(1000);
     runner.init();
     runner.addTask(controlTask);
     controlTask.enable();
+    
+    Serial.println("Holi");
 }
 
 
 void loop() {
+    mpuRead();
+    spiRead();
+    
+    runner.execute();
+}
+
+void spiRead(){
+    if (spiInterrupt) { 
+        Serial.println("spiRead.spiInterrupt");
+        // Parity check
+        byte parityByte = spiBuffer[pos-1];
+        Serial.println(parityByte);
+        byte parity = 0;
+        for (int i=0; i<pos-1; i++){
+          Serial.print(spiBuffer[i],HEX);
+          parity += spiBuffer[i];
+        }
+        Serial.println();
+        
+
+        // Data received correctly
+        if (parityByte == parity){
+          Serial.println ('!');
+          Serial.println (spiBuffer[0]);
+          //linearVelocityRef = 0;
+          if (spiBuffer[0] == 'M'){
+            linearVelocityRef = (spiBuffer[1]<<8) | spiBuffer[2] & 0xFF;
+            angularVelocityRef = (spiBuffer[3]<<8) | spiBuffer[4] & 0xFF;
+            
+            Serial.println (linearVelocityRef); 
+            Serial.println (angularVelocityRef);  
+          }
+        }
+
+        // Reset Bufffer
+        pos = 0;
+        spiInterrupt = false;
+    }
+}
+void mpuRead(){
     if (mpuInterrupt) {
         mpuInterrupt = false;
         int mpuIntStatus = mpu.getIntStatus();
@@ -89,62 +159,97 @@ void loop() {
             mpu.resetFIFO();
         }
     }
-    
-    runner.execute();
 }
 
+float getAngularVelocity(float yaw){
+    static float lastYaw;
 
-float getYaw() {
-    noInterrupts();
-    int w = ((fifoBuffer[0] << 8) | fifoBuffer[1]);
-    int x = ((fifoBuffer[4] << 8) | fifoBuffer[5]);
-    int y = ((fifoBuffer[8] << 8) | fifoBuffer[9]);
-    int z = ((fifoBuffer[12] << 8) | fifoBuffer[13]);
-    interrupts();
+    //float yaw = getYaw();
 
+    // Yaw overflows [-180:180]
+    float yawDifference = yaw - lastYaw;
+    
+    if (yawDifference > 180)
+      yawDifference -= 360;
+    else if (yawDifference < -180)
+      yawDifference += 360;
+
+    float angularVelocity = (yawDifference)*1000/sampling_time;
+
+    lastYaw = yaw;
+
+    return angularVelocity;
+}
+
+float getYaw(){
+    //noInterrupts();
     mpu.dmpGetQuaternion(&q, fifoBuffer);
+    //interrupts();
+    
     float yaw = atan2(2*q.x*q.y - 2*q.w*q.z, 2*q.w*q.w + 2*q.x*q.x - 1) * 180/M_PI;
     
-    return yaw;
+    return -yaw;
 }
 
 // Reference values in range [-1024:+1024] aprox
 void setReference(int v, int w){
- int leftMotorSpeed=constrain( (v-w)/4 ,-255,255);
- int rightMotorSpeed=constrain( (v+w)/4 ,-255,255);
- 
- setRightMotorSpeed(rightMotorSpeed);
- setLeftMotorSpeed(leftMotorSpeed);
+    int leftMotorSpeed=constrain( (v-w)/4 ,-255,255);
+    int rightMotorSpeed=constrain( (v+w)/4 ,-255,255);
+    
+//    Serial.print(rightMotorSpeed);
+//    Serial.print('\t');
+//    Serial.println(leftMotorSpeed);
+
+    setRightMotorSpeed(rightMotorSpeed);
+    setLeftMotorSpeed(leftMotorSpeed);
 }
 
 void setRightMotorSpeed(int speed){
- if (speed>0){
-   digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,LOW);
-   analogWrite(MOTOR_RIGHT_FORWARDS_PIN,speed);
- } else if (speed<0){
-   digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,LOW);
-   analogWrite(MOTOR_RIGHT_BACKWARDS_PIN,-speed);
- } else{
-   digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,LOW);
-   digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,LOW);
- }
+    if (speed>0){
+        digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,HIGH);
+        digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,LOW);
+    } else if (speed<0){
+        digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,LOW);
+        digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,HIGH);
+    } else{
+        digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,LOW);
+        digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,LOW);
+    }
+    analogWrite(MOTOR_RIGHT_ENABLE_PIN, abs(speed));
 }
 
 void setLeftMotorSpeed(int speed){
- if (speed>0){
-   digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,LOW);
-   analogWrite(MOTOR_LEFT_FORWARDS_PIN,speed);
- } else if (speed<0){
-   digitalWrite(MOTOR_LEFT_FORWARDS_PIN,LOW);
-   analogWrite(MOTOR_LEFT_BACKWARDS_PIN,-speed);
- } else{
-   digitalWrite(MOTOR_LEFT_FORWARDS_PIN,LOW);
-   digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,LOW);
- }
+    if (speed>0){
+        digitalWrite(MOTOR_LEFT_FORWARDS_PIN,HIGH);
+        digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,LOW);
+    } else if (speed<0){
+        digitalWrite(MOTOR_LEFT_FORWARDS_PIN,LOW);
+        digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,HIGH);
+    } else{
+        digitalWrite(MOTOR_LEFT_FORWARDS_PIN,LOW);
+        digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,LOW);
+    }
+    analogWrite(MOTOR_LEFT_ENABLE_PIN, abs(speed));
 }
 
 
 /************* SETUP ROUTINES *************/
+
+void spiSetup(){
+  // turn on SPI in slave mode
+  SPCR |= bit (SPE);
+
+  // have to send on master in, *slave out*
+  pinMode(MISO, OUTPUT);
+  
+  // get ready for an interrupt 
+  pos = 0;   // Buffer empty
+  spiInterrupt = false;
+
+  // now turn on interrupts
+  SPI.attachInterrupt();
+  attachInterrupt(digitalPinToInterrupt(SPI_INTERRUPT_PIN), spiDataReady, RISING);
+}
 
 void mpuSetup() {
     Wire.begin();
@@ -153,7 +258,7 @@ void mpuSetup() {
     // initialize device
     Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
-    pinMode(INTERRUPT_PIN, INPUT);
+    pinMode(I2C_INTERRUPT_PIN, INPUT);
 
     // verify connection
     Serial.println(F("Testing device connections..."));
@@ -164,12 +269,12 @@ void mpuSetup() {
     mpu.dmpInitialize();
 
     // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setXAccelOffset(5);
-    mpu.setYAccelOffset(10);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+    mpu.setXAccelOffset(-3060);
+    mpu.setYAccelOffset(-1733);
+    mpu.setZAccelOffset(1480);
+    mpu.setXGyroOffset(82);
+    mpu.setYGyroOffset(22);
+    mpu.setZGyroOffset(6);
 
     // turn on the DMP, now that it's ready
     Serial.println(F("Enabling DMP..."));
@@ -177,7 +282,7 @@ void mpuSetup() {
 
     // enable Arduino interrupt detection
     Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    attachInterrupt(digitalPinToInterrupt(I2C_INTERRUPT_PIN), dmpDataReady, RISING);
 
     // set our DMP Ready flag so the main loop() function knows it's okay to use it
     Serial.println(F("DMP ready! Waiting for first interrupt..."));
@@ -185,10 +290,17 @@ void mpuSetup() {
 }
 
 void motorsSetup() {
+    
+    pinMode(MOTOR_RIGHT_ENABLE_PIN,OUTPUT);
+    pinMode(MOTOR_LEFT_ENABLE_PIN,OUTPUT);
+    delay(500);
+    
     pinMode(MOTOR_RIGHT_FORWARDS_PIN,OUTPUT);
     pinMode(MOTOR_RIGHT_BACKWARDS_PIN,OUTPUT);
+
     pinMode(MOTOR_LEFT_FORWARDS_PIN,OUTPUT);
     pinMode(MOTOR_LEFT_BACKWARDS_PIN,OUTPUT);
+    
 }
 
 void ledsSetup() {
---
+++ old/Cybor_reborn.ino
@@ -0,0 +1,108 @@
+#include <Wire.h>
+
+// H-Bridge motor pins                      7 pin socket driver
+const byte MOTOR_RIGHT_FORWARDS_PIN = 11;   // 1. Blue
+const byte MOTOR_RIGHT_BACKWARDS_PIN = 10;  // 2. Grey
+const byte MOTOR_LEFT_FORWARDS_PIN = 9;     // 3. Green
+const byte MOTOR_LEFT_BACKWARDS_PIN = 6;    // 4. White// LED Antennas
+const int LED_LEFT_PIN = 2;
+const int LED_RIGHT_PIN = 3;
+
+// Robot variables
+boolean ledLeft = false;
+boolean ledRight = false;
+int linearVelocity = 0;
+int angularVelocity = 0;
+
+
+void setup() {
+ //Serial.begin(9600);
+ 
+ pinMode(MOTOR_RIGHT_FORWARDS_PIN,OUTPUT);
+ pinMode(MOTOR_RIGHT_BACKWARDS_PIN,OUTPUT);
+ pinMode(MOTOR_LEFT_FORWARDS_PIN,OUTPUT);
+ pinMode(MOTOR_LEFT_BACKWARDS_PIN,OUTPUT);
+
+ pinMode(LED_LEFT_PIN,OUTPUT);
+ pinMode(LED_RIGHT_PIN,OUTPUT);
+
+ Wire.begin(8);           // join i2c bus with address #8
+ Wire.onReceive(readI2C); // register event
+ 
+ //Serial.println("Hola");
+}
+
+void loop() {
+ digitalWrite(LED_LEFT_PIN,ledLeft);
+ digitalWrite(LED_RIGHT_PIN,ledRight);
+ reference(linearVelocity, angularVelocity);
+}
+
+
+void readI2C(int howMany) {
+ char cmd = Wire.read();
+ if (Wire.available()){  // More data
+   byte result[howMany-1];
+   int i = 0;
+   while (Wire.available()) {
+     result[i] = Wire.read();
+     i++;
+   }
+   switch (cmd){
+     case 'L': updateLeds(result[0]); break;
+     case 'M': updateMotors(result); break;
+     default: break;
+   }
+ }
+}
+
+void updateLeds(byte result){
+ ledLeft = result & B00000010;
+ ledRight = result & B00000001;
+}
+
+void updateMotors(byte result[]){
+ linearVelocity = (result[0]<<8) + result[1];
+ angularVelocity = (result[2]<<8) + result[3];
+}
+
+void reference(int v, int w){
+ v = v/80;
+ w = w/20;
+ int leftMotor=constrain( v-w ,-255,255);
+ int rightMotor=constrain( v+w ,-255,255);
+ 
+ motorSpeed(leftMotor,rightMotor);
+}
+
+
+void motorSpeed(int left, int right){
+ leftMotorSpeed(left);
+ rightMotorSpeed(right);
+}
+
+void rightMotorSpeed(int speed){
+ if (speed>0){
+   digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,LOW);
+   analogWrite(MOTOR_RIGHT_FORWARDS_PIN,speed);
+ } else if (speed<0){
+   digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,LOW);
+   analogWrite(MOTOR_RIGHT_BACKWARDS_PIN,-speed);
+ } else{
+   digitalWrite(MOTOR_RIGHT_FORWARDS_PIN,LOW);
+   digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN,LOW);
+ }
+}
+
+void leftMotorSpeed(int speed){
+ if (speed>0){
+   digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,LOW);
+   analogWrite(MOTOR_LEFT_FORWARDS_PIN,speed);
+ } else if (speed<0){
+   digitalWrite(MOTOR_LEFT_FORWARDS_PIN,LOW);
+   analogWrite(MOTOR_LEFT_BACKWARDS_PIN,-speed);
+ } else{
+   digitalWrite(MOTOR_LEFT_FORWARDS_PIN,LOW);
+   digitalWrite(MOTOR_LEFT_BACKWARDS_PIN,LOW);
+ }
+}
